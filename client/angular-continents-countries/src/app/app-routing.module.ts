e5361d8b4ff8a59620f3c4b59230cf83f45d827a
yarden <yarden@bis-tec.co.il>
2020-10-03 13:18:57 +0300

    add routing

--- client/angular-continents-countries/src/app/app-routing.module.ts
+++ client/angular-continents-countries/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CountriesComponent } from './components/countries/countries/countries.component';
+import { ContinentsComponent } from './containers/continents/continents/continents.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContinentsComponent,
+  },
+  {path: ':continent', component: CountriesComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
