022a7c95c85c23d8bc3288485ba46e6e1a433004
yarden <yarden@bis-tec.co.il>
2020-10-03 14:11:08 +0300

    add nxg table

--- client/angular-continents-countries/src/app/app.module.ts
+++ client/angular-continents-countries/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { CountriesComponent } from './components/countries/countries/countries.component';
+import { ContinentsComponent } from './containers/continents/continents/continents.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ContinentsComponent, CountriesComponent
   ],
   imports: [
+    NgxDatatableModule ,
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+  // exports:[ContinentsComponent, CountriesComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/angular-continents-countries/src/app/containers/continents/continents/continents.component.ts
+++ client/angular-continents-countries/src/app/containers/continents/continents/continents.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-continents',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./continents.component.css']
 })
 export class ContinentsComponent implements OnInit {
+  rows = [
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Molly', gender: 'Female', company: 'Burger King' }
+  ];
+  columns = [{ prop: 'name' }, { name: 'Gender' }, { name: 'Company' }];
 
-  constructor() { }
+  constructor(private router: Router,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  public onActivate(event) {
+
+    if(event.type === "click" ) {
+      const name = event.row.name;
+      this.router.navigate(['', name], { relativeTo: this.route });
+    }
+
   }
 
 }
